f058adbb89b12f059dce58b8fb73eed80adbb6c9
Solo un tostapane <33027100+JustAToaster@users.noreply.github.com>
2022-07-17 18:10:53 +0200

    Update start_services.sh

--- kubernetes-cluster/start_services.sh
+++ kubernetes-cluster/start_services.sh
@@ -2,9 +2,10 @@
 
 set -e -o pipefail
 
-echo "----------- 1. Loading services from online manifests -----------"
+#echo "----------- 1. Loading services from online manifests -----------"
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.1/deploy/static/provider/cloud/deploy.yaml
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
 
-echo "----------- 2. Loading services contained in the local folder -----------"
+echo "----------- Loading services contained in the local folder -----------"
 kubectl apply -f ./services
+echo "----------- DONE -----------"
