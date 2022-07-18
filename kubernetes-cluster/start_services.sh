c8d53f32286046f0480ed46f49a82a39ed3a9d0b
JustAToaster <danipcmedia@gmail.com>
2022-07-18 11:11:37 +0200

    Add nodes internet fix to start_services.sh

--- kubernetes-cluster/start_services.sh
+++ kubernetes-cluster/start_services.sh
@@ -2,10 +2,10 @@
 
 set -e -o pipefail
 
-#echo "----------- 1. Loading services from online manifests -----------"
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.1/deploy/static/provider/cloud/deploy.yaml
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml
+echo "----------- 0. Fixing coreDNS clusterRole -----------"
+kubectl patch clusterrole system:coredns -n kube-system --type='json' -p='[{"op": "replace", "path": "/rules/0/resources", "value":["nodes", "endpoints", "services", "pods", "namespaces"]}]'
+echo "Done".
 
-echo "----------- Loading services contained in the local folder -----------"
+echo "----------- 1. Loading services contained in the local folder -----------"
 kubectl apply -f ./services
 echo "----------- DONE -----------"
