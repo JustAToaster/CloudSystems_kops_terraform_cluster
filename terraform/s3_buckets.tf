d9cffb2a85cc06483554541d0acb08020a13fbe1
JustAToaster <danipcmedia@gmail.com>
2023-02-15 18:54:49 +0100

    Use single bucket for YOLOv5 stuff

--- terraform/s3_buckets.tf
+++ terraform/s3_buckets.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   acl    = "public-read-write"
 }
 
-// S3 bucket to store the YOLOv5 models and the report list
+// S3 bucket to store the YOLOv5 models, training data and the report list
 resource "aws_s3_bucket" "models_data" {
   bucket        = local.models_data_bucket_name
   force_destroy = true
@@ -22,35 +22,11 @@ resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   acl    = "public-read-write"
 }
 
-// S3 bucket to store the training data for YOLOv5 models
-resource "aws_s3_bucket" "training_data" {
-  bucket        = local.training_data_bucket_name
-  force_destroy = true
-  tags          = "${merge(local.tags)}"
-}
-
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket.training_data.bucket
-  acl    = "public-read-write"
-}
-
-
-//Upload data to S3 buckets
-
-//To models data bucket
+//Upload data to S3 bucket
 resource "aws_s3_object" "s3_object_models" {
   for_each = fileset("./s3_data/", "**")
   bucket = aws_s3_bucket.models_data.id
   key = each.value
-  source = "./s3_data/models_bucket/${each.value}"
-  etag = filemd5("./s3_data/models_bucket/${each.value}")
-}
-
-//To training data bucket
-resource "aws_s3_object" "s3_object_models" {
-  for_each = fileset("./s3_data/", "**")
-  bucket = aws_s3_bucket.training_data.id
-  key = each.value
-  source = "./s3_data/training_bucket/${each.value}"
-  etag = filemd5("./s3_data/training_bucket/${each.value}")
+  source = "./s3_data/${each.value}"
+  etag = filemd5("./s3_data/${each.value}")
 }
